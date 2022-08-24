30769faa9ed2a395dd23b0efdb755fcad948770e
vitor-henridias <vhsilva.mg@gmail.com>
2022-08-23 21:18:31 -0300

    Knex.js Tutorial For Beginners

---
+++ db/migrations/20220823233907_create_user_table.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('users', function (table){
+        table.increments('id');
+        table.string('name', 255).notNullable();
+        table.string('email', 255);
+        table.timestamps();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema
+    .dropTable('users');
+  
+};
